5563f82c334730a553b920ca014b78065be2168e
julio.barron <julio.barron@e32.tech>
2020-09-05 18:53:14 -0500

    Initial Commit

---
+++ Contactos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contactos
+//
+//  Created by IOS Developer on 05/09/20.
+//  Copyright © 2020 JEBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
