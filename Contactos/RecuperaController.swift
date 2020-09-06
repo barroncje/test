df85fecb7d48b5085b800c045ce9ecc77a9c42ea
julio.barron <julio.barron@e32.tech>
2020-09-05 20:44:36 -0500

    90 minutos

---
+++ Contactos/RecuperaController.swift
@@ -0,0 +1,30 @@
+//
+//  RecuperaController.swift
+//  Contactos
+//
+//  Created by IOS Developer on 05/09/20.
+//  Copyright © 2020 JEBC. All rights reserved.
+//
+
+import UIKit
+
+class RecuperaController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Contactos/celdaContacto.swift
@@ -0,0 +1,24 @@
+//
+//  celdaContacto.swift
+//  Contactos
+//
+//  Created by IOS Developer on 05/09/20.
+//  Copyright © 2020 JEBC. All rights reserved.
+//
+
+import UIKit
+
+class celdaContacto: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
